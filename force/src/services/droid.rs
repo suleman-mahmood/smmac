706b18a486c283e578e4ce31ad96b82dee218c19
Suleman Mahmood <sulemanmahmood99@gmail.com>
2024-12-04 12:45:06 +0500

    added proxy code

--- force/src/services/droid.rs
+++ force/src/services/droid.rs
@@ -1,4 +1,7 @@
-use thirtyfour::{DesiredCapabilities, WebDriver};
+use rand::seq::SliceRandom;
+use thirtyfour::{CapabilitiesHelper, DesiredCapabilities, Proxy, WebDriver};
+
+const PROXY_LIST: [&str; 1] = ["http://uzuugbox-rotate:mgbqddrmu9fi@p.webshare.io:80/"];
 
 pub struct Droid {
     pub driver: WebDriver,
@@ -7,6 +10,36 @@ pub struct Droid {
 impl Droid {
     pub async fn new() -> Self {
         let caps = DesiredCapabilities::chrome();
+
+        // http://chrome:4444/wd/hub
+        // http://localhost:58656
+        let driver = WebDriver::new("http://localhost:62510", caps)
+            .await
+            .unwrap();
+        driver.maximize_window().await.unwrap();
+
+        Droid { driver }
+    }
+
+    pub async fn new_driver_from_proxy() -> Self {
+        let mut caps = DesiredCapabilities::chrome();
+
+        let proxy = Proxy::Manual {
+            ftp_proxy: None,
+            http_proxy: Some(
+                PROXY_LIST
+                    .choose(&mut rand::thread_rng())
+                    .unwrap()
+                    .to_string(),
+            ),
+            ssl_proxy: None,
+            socks_proxy: None,
+            socks_version: None,
+            socks_username: None,
+            socks_password: None,
+            no_proxy: None,
+        };
+        caps.set_proxy(proxy).unwrap();
 
         // http://chrome:4444/wd/hub
         // http://localhost:58656
