5f9bf0ede5dfdd102bf0c6151fbc09b1d6fe5850
Suleman Mahmood <sulemanmahmood99@gmail.com>
2024-12-27 22:00:05 +0500

    made migration faster

--- force/src/routes/exp_route.rs
+++ force/src/routes/exp_route.rs
@@ -1,5 +1,4 @@
 use actix_web::{get, web, HttpResponse};
-use reqwest::header::ValueIter;
 use sqlx::PgPool;
 
 use crate::{
@@ -70,26 +69,49 @@ async fn migrate(pool: web::Data<PgPool>) -> HttpResponse {
     .await
     .unwrap();
 
+    let total_rows = rows.len();
+    let founder_names: Vec<String> = (0..total_rows)
+        .map(|_| "before-migration-founder-name".to_string())
+        .collect();
+    let domains: Vec<String> = (0..total_rows)
+        .map(|_| "before-migration-domain".to_string())
+        .collect();
+
+    let mut email_addresses = Vec::new();
+    let mut statuses = Vec::new();
+    let mut reaches = Vec::new();
+
     for r in rows {
         let status: VerificationStatus = r.verified_status.into();
         let reach: Reachability = r.reachability.into();
 
-        if let Err(e) = sqlx::query!(
-            r"
-            insert into email
-                (email_address, verification_status, reachability, founder_name, domain)
-            values
-                ($1, $2, $3, 'before-migration-founder-name', 'before-migration-domain')
-            ",
-            r.email_address,
-            status as VerificationStatus,
-            reach as Reachability,
+        email_addresses.push(r.email_address);
+        statuses.push(status);
+        reaches.push(reach);
+    }
+
+    if let Err(e) = sqlx::query!(
+        r"
+        insert into email
+            (email_address, verification_status, reachability, founder_name, domain)
+        select * from unnest (
+            $1::text[],
+            $2::VerificationStatus[],
+            $3::Reachability[],
+            $4::text[],
+            $5::text[]
         )
-        .execute(pool.as_ref())
-        .await
-        {
-            log::error!("Error inserting into new email table from old: {:?}", e);
-        }
+        ",
+        &email_addresses,
+        statuses as Vec<VerificationStatus>,
+        reaches as Vec<Reachability>,
+        &founder_names,
+        &domains,
+    )
+    .execute(pool.as_ref())
+    .await
+    {
+        log::error!("Error inserting into new email table from old: {:?}", e);
     }
 
     HttpResponse::Ok().body("Done")
