03cb6e6653dfe47a32341bdd000ffe52e89813d0
Suleman Mahmood <sulemanmahmood99@gmail.com>
2024-12-27 13:34:08 +0500

    added db for data extract and html tag

--- force/src/dal/data_extract_db.rs
+++ force/src/dal/data_extract_db.rs
@@ -2,10 +2,35 @@ use sqlx::PgConnection;
 
 use crate::domain::data_extract::DataExtract;
 
+#[derive(sqlx::Type)]
+#[sqlx(type_name = "DataType", rename_all = "SCREAMING_SNAKE_CASE")]
+enum DataType {
+    Domain,
+    FounderName,
+}
+
 pub async fn insert_data(
     con: &mut PgConnection,
     data: DataExtract,
     tag_id: i64,
 ) -> Result<i64, sqlx::Error> {
-    todo!()
+    let (content, data_type) = match data {
+        DataExtract::Domain(content) => (content, DataType::Domain),
+        DataExtract::FounderName(content) => (content, DataType::FounderName),
+    };
+
+    sqlx::query_scalar!(
+        r"
+        insert into data_extract
+            (data, data_type, html_tag_id)
+        values
+            ($1, $2, $3)
+        returning id
+        ",
+        content,
+        data_type as DataType,
+        tag_id,
+    )
+    .fetch_one(&mut *con)
+    .await
 }
--- force/src/dal/html_tag_db.rs
+++ force/src/dal/html_tag_db.rs
@@ -2,10 +2,39 @@ use sqlx::PgConnection;
 
 use crate::domain::html_tag::HtmlTag;
 
+#[derive(sqlx::Type)]
+#[sqlx(type_name = "HtmlTagType", rename_all = "SCREAMING_SNAKE_CASE")]
+enum HtmlTagType {
+    ATag,
+    H3Tag,
+    SpanTag,
+    NextPageATag,
+}
+
 pub async fn insert_html_tag(
     con: &mut PgConnection,
     html_tag: HtmlTag,
     web_page_id: i64,
 ) -> Result<i64, sqlx::Error> {
-    todo!()
+    let (content, tag_type) = match html_tag {
+        HtmlTag::ATag(content) => (content, HtmlTagType::ATag),
+        HtmlTag::H3Tag(content) => (content, HtmlTagType::H3Tag),
+        HtmlTag::SpanTag(content) => (content, HtmlTagType::SpanTag),
+        HtmlTag::NextPageATag(content) => (content, HtmlTagType::NextPageATag),
+    };
+
+    sqlx::query_scalar!(
+        r"
+        insert into html_tag
+            (text_content, html_tag_type, google_webpage_id)
+        values
+            ($1, $2, $3)
+        returning id
+        ",
+        content,
+        tag_type as HtmlTagType,
+        web_page_id,
+    )
+    .fetch_one(&mut *con)
+    .await
 }
